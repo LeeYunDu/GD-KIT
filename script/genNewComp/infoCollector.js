ff026855883dc7e756664c51c4993a73f2121a7f
LeeYunDu <492107910@qq.com>
2022-04-01 16:31:47 +0800

    feat() 快捷创建组件:正则校验修改为不为空即可

--- script/genNewComp/infoCollector.js
+++ script/genNewComp/infoCollector.js
@@ -6,13 +6,14 @@ const listFilePath = '../../packages/list.json'
 
 const RegxMap = {
   IS_COMP_NAME: /^[A-Z]/,
+  IS_NOT_NULL:/^.+$/,
   IS_COMP_ZH_NAME: /^(?:[\u3400-\u4DB5\u4E00-\u9FEA\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\uD840-\uD868\uD86A-\uD86C\uD86F-\uD872\uD874-\uD879][\uDC00-\uDFFF]|\uD869[\uDC00-\uDED6\uDF00-\uDFFF]|\uD86D[\uDC00-\uDF34\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1D\uDC20-\uDFFF]|\uD873[\uDC00-\uDEA1\uDEB0-\uDFFF]|\uD87A[\uDC00-\uDFE0])+$/
 }
 
 const kebabCase = string => string
-  .replace(/([a-z])([A-Z])/g, "$1-$2")
+  .replace(/([a-z])([A-Z])/g, '$1-$2')
   .replace(/[\s_]+/g, '-')
-  .toLowerCase();
+  .toLowerCase()
 
 module.exports = async () => {
   const meta = await inquirer
@@ -21,7 +22,7 @@ module.exports = async () => {
         type: 'input',
         message: '请输入你要新建的组件名（纯英文，大写开头）：',
         name: 'compName',
-        validate(answer) {
+        validate (answer) {
           const done = this.async()
           const validateRes = RegxMap.IS_COMP_NAME.test(answer)
           if (!validateRes) {
@@ -40,9 +41,9 @@ module.exports = async () => {
         type: 'input',
         message: '请输入你要新建的组件名（中文）：',
         name: 'compZhName',
-        validate(answer) {
+        validate (answer) {
           const done = this.async()
-          const validateRes = RegxMap.IS_COMP_ZH_NAME.test(answer)
+          const validateRes = RegxMap.IS_NOT_NULL.test(answer)
           if (!validateRes) {
             done('请按要求输入正确的组件名！')
             return
